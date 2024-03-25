3846bc29bd460c676e8e72194fa8a2a80a7eab0f
fernando <maruno.string@gmail.com>
2024-03-25 14:08:18 -0300

    imports que utilizaremos no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+// Para trabalhar com FORMULÁRIOS no Angular 12
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Para realizar REQUISIÇÕES HTTP para o nosso Backend
+import { HttpClientModule } from '@angular/common/http';
+
+// Todos os Imports dos COMPONENTES do Angular Material que iremos utilizar
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+ 
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +36,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // FORMULÁRIOS
+    FormsModule,
+    ReactiveFormsModule,
+    // REQUISIÇÕES http
+    HttpClientModule,
+    // COMPONENTES Angular Material
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatTableModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
